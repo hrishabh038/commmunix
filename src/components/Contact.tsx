7dfe6a19d3b3d8759ce863446d3b0486acbdd398
Hrishabh Jain <hrishabh038@gmail.com>
2024-02-29 08:53:22 +0530

    Fourth Commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,10 +9,13 @@ const Contact = (props: Props) => {
     <div className="bg_company_gradient_only px-4 py-10 rounded-lg text-white ">
       <div className="text-center flex flex-col items-center justify-center gap-6 w-full sm:w-[600px] m-auto">
         <h1 className="text-3xl tracking-wide font-bold">
-        Communication is the Key
+          Communication is the Key
         </h1>
         <p className="text-center">
-        We strive to stay in communication with our clients. Have a question about our business, or want to see if we match your specific needs? Send us a message, or give us a call. We're always happy to meet new customers!
+          We strive to stay in communication with our clients. Have a question
+          about our business, or want to see if we match your specific needs?
+          Send us a message, or give us a call. We&apos;re always happy to meet
+          new customers!
         </p>
         <button className="duration-200 pr-5 bg-white scale-105 hover:scale-100 rounded-lg font-semibold flex items-center justify-center">
           <div className="relative w-[70px] aspect-square rounded-lg">
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const ServiceCard = ({title,para, icon}: Props) => {
   return (
-    <div className="w-full  border hover:border-orange-500 rounded-lg bg-white p-5 flex flex-col gap-6">
+    <div className="w-full  border hover:border-orange-500 rounded-lg bg-white p-5 flex flex-col gap-6 shadow">
       <HomeIcon className="w-[40px] h-[40px] text-orange-500" />
       <div className="flex flex-col gap-2">
         <h1 className="text-xl font-semibold">{title}</h1>
