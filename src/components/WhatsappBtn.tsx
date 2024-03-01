f28a03206c274858719391ae21003840db6092b9
Hrishabh Jain <hrishabh038@gmail.com>
2024-03-01 14:30:08 +0530

    Commit-2 v1

--- src/components/WhatsappBtn.tsx
+++ src/components/WhatsappBtn.tsx
@@ -6,7 +6,7 @@ type Props = { text?: string };
 
 const WhatsappBtn = (props: Props) => {
   return (
-    <a href="https://api.whatsapp.com/send/?phone=919692552656&text&type=phone_number&app_absent=0">
+    <a className="w-fit" href="https://api.whatsapp.com/send/?phone=919692552656&text&type=phone_number&app_absent=0">
       <button className="duration-200 pr-5 bg-[#F4F4F4] scale-105 hover:scale-100 rounded-lg font-semibold flex items-center justify-center border">
         <div className="relative w-[70px] aspect-square rounded-lg">
           <Image className="rounded-lg" src={whatsapp} alt="Whatsapp" fill />
